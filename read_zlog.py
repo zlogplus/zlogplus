c9c84a010000dffdd95116b4a388e5fef6440793
zlogplus <zlogplus@gmail.com>
2019-05-11 00:33:08 +0900

    start reading .zlog file

--- read_zlog.py
+++ read_zlog.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 
-def read_zlog(filename):
-	filename = str(filename)
-  zlogfile = open(filename, 'r+b')
+import codecs
 
+def read_zlog(filename):
+    filename = str(filename)
+    zlogfile = open(filename, 'r+b')
+    while True:
+        chunk = zlogfile.read(256)
+        if not chunk:
+            break
+        print(chunk.hex())
 
 
 if __name__ == "__main__":
-	filename = input("filename: ")
-	print(read_zlog(filename))
+    filename = input("filename: ")
+    print(read_zlog(filename))
